250cf0d50728d02d00cc96ba9fa83930371f10d6
AbhilakshSinghReen <abhilakshreen@gmail.com>
2023-12-12 16:21:10 +0530

    Report simplification stub

---
+++ radio_reports_api/task_scripts/get_simplified_reports/__init__.py
@@ -0,0 +1,1 @@
+from .get_simplified_reports import get_simplified_report_data_for_languages
--- radio_reports_api/tasks.py
+++ radio_reports_api/tasks.py
@@ -12,14 +12,12 @@ from radio_reports_api.cache import (
 from radio_reports.settings import CACHE_ROOT
 from radio_reports_api.task_scripts.nifti_to_segmentation import run_total_segmentator_on_nii_image
 from radio_reports_api.task_scripts.segmentation_to_mesh import total_segmentator_output_to_gltf
+from radio_reports_api.task_scripts.get_simplified_reports import get_simplified_report_data_for_languages
 from radio_reports_api.utils import unique_str
 from radio_reports_api.models import Report
 
 
-# get_simplified_report = ",,,"
-# get_segments_of_interest = "wdq"
-
-default_report_simplification_languages = ["english", "hindi"]
+default_report_simplification_languages = ["English", "Hindi"]
 
 def generate_report_text_and_model(
         report_id,
@@ -42,40 +40,6 @@ def generate_report_text_and_model(
     report_object.processing_status = "Starting Task..."
     report_object.save()
 
-    simplified_reports = {}
-
-    # for language in default_report_simplification_languages:
-    #     try:
-    #         simplified_reports[language] = get_simplified_report(report_data, language)
-    #     except:
-    #         task_status = f"Failed at report simplification for language: {language}."
-    #         report_object.processing_status = task_status
-    #         report_object.save()
-    #         print(task_status)
-    #         return task_status
-        
-    report_object.meshes_metadata = json.dumps(simplified_reports)
-    report_object.processing_status = "Getting segments of interest..."
-    report_object.save()
-
-    try:
-        # segments_of_interest = get_segments_of_interest(report_data)
-        segments_of_interest = [
-            'rib_left_10',
-            'spleen',
-            'foo_bar_that_does_not_exist',
-            'adrenal_gland_left',
-        ]
-    except:
-        task_status = f"Failed to get segments of interest."
-        report_object.processing_status = task_status
-        report_object.save()
-        print(task_status)
-        return task_status
-    
-    report_object.processing_status = "Running Total Segmentator on nii image..."
-    report_object.save()
-
     try:
         run_total_segmentator_on_nii_image(nii_file_path, ts_out_file_path, nifti_file_name)
     except:
@@ -91,7 +55,7 @@ def generate_report_text_and_model(
     # try:
     meshes_metadata = total_segmentator_output_to_gltf(ts_out_file_path, meshes_output_dir_path, nifti_file_name)
     # print(meshes_metadata)
-    meshes_metadata['segmentsOfInterest'] = segments_of_interest
+    
     # except:
     #     task_status = f"Failed to generate glTF from segmentation."
     #     report_object.processing_status = task_status
@@ -127,6 +91,40 @@ def generate_report_text_and_model(
         report_object.save()
         print(task_status)
         return task_status
+    
+    report_object.processing_status = "Generating simplified reports."
+    report_object.save()
 
+    try:
+        simplified_reports = get_simplified_report_data_for_languages(report_data, default_report_simplification_languages)
+        # print(simplified_reports)
+    except:
+        task_status = f"Failed to generate simplified reports."
+        report_object.processing_status = task_status
+        report_object.save()
+        print(task_status)
+        return task_status
+        
+    report_object.simplified_reports = json.dumps(simplified_reports)
+    report_object.processing_status = "Getting segments of interest..."
+    report_object.save()
+
+    try:
+        # segments_of_interest = get_segments_of_interest(report_data)
+        segments_of_interest = [
+            'rib_left_10',
+            'spleen',
+            'foo_bar_that_does_not_exist',
+            'adrenal_gland_left',
+        ]
+    except:
+        task_status = f"Failed to get segments of interest."
+        report_object.processing_status = task_status
+        report_object.save()
+        print(task_status)
+        return task_status
+
+    meshes_metadata['segmentsOfInterest'] = segments_of_interest
+    report_object.meshes_metadata = json.dumps(meshes_metadata)
     report_object.processing_status = "Processing completed."
     report_object.save()
